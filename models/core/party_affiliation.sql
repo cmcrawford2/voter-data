a24aaccd42a6041e1bde4b2a834fbf2e5e075c06
Cris Crawford <crawford.cris@gmail.com>
2024-03-30 15:56:10 +0000

    "Added seed for party designation"

--- models/core/party_affiliation.sql
+++ models/core/party_affiliation.sql
@@ -1,9 +1,9 @@
 -- File: models/party_affiliations.sql
 
 -- Define a seed model for party affiliations
-{{ config(materialized='seed') }}
+{{ config(materialized='table') }}
 
 select
     'Code' as code,
     'Party or Designation Name' as party_name
-from source('data', 'PartyDesignationCodes.csv')
+from {{ ref('PartyDesignationCodes') }}
